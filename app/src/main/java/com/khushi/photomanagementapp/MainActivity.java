cc8989ffd05782432b88dd770ea80cec12d57adb
Khushi <khushi.rani1611@gmail.com>
2023-07-07 02:58:28 +0530

    Second Commit

--- app/src/main/java/com/khushi/photomanagementapp/MainActivity.java
+++ app/src/main/java/com/khushi/photomanagementapp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +15,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private final int GALLERY_REQ_CODE = 1000;
+    private final int CAMERA_REQ_CODE = 200;
     ImageView imgGallery;
 
     @Override
@@ -22,13 +25,25 @@ public class MainActivity extends AppCompatActivity {
 
         imgGallery= findViewById(R.id.imgGallery);
         Button btnGallery = findViewById(R.id.btnGallery);
+        Button btnCamera = findViewById(R.id.btnCamera);
+
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(iCamera, CAMERA_REQ_CODE);
+            }
+        });
+
+        Log.e("TAG", "onCreate: ",null );
 
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 Intent iGallery = new Intent(Intent.ACTION_PICK);
-                 iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                 startActivityForResult(iGallery, GALLERY_REQ_CODE);
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQ_CODE);
 
             }
         });
@@ -44,6 +59,12 @@ public class MainActivity extends AppCompatActivity {
             {
                 // for gallery
                 imgGallery.setImageURI(data.getData());
+            }
+            else if (requestCode == CAMERA_REQ_CODE)
+            {
+                // for Camera
+                Bitmap img =(Bitmap)(data.getExtras().get("data"));
+                imgGallery.setImageBitmap(img);
 
             }
         }
